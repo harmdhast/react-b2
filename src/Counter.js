04c710016e0b8c8bafd1256de0d4238fbee3ef8b
harmdhast <pandemonium.harmdhast@gmail.com>
2024-01-29 17:06:46 +0100

    Transition fonctionnelle

--- src/Counter.js
+++ src/Counter.js
@@ -1,19 +1,28 @@
 import { useState } from "react";
 import { PlusIcon, MinusIcon } from "@radix-ui/react-icons"
+import { useTransition, animated, to } from '@react-spring/web'
 
 import { Button } from "@/components/ui/button"
 
 function Counter({ count, setCount }) {
     const [isError, setIsError] = useState(false);
+    const [forward, setForward] = useState(true);
 
-    function changeCount(test) {
-        if (count + test < 0) {
+    function changeCount(val) {
+        if (count + val < 0) {
             return setIsError(true);
         }
-
-        setCount(count + test);
+        setForward(val < 0);
+        setCount(count + val);
         setIsError(false);
     }
+
+    const transitions = useTransition([count], {
+        from: { position: forward ? '100%' : '-100%', opacity: 0, rotation: '90deg' },
+        enter: { position: '0%', opacity: 1, rotation: '0deg' },
+        leave: { position: forward ? '-100%' : '100%', opacity: 0, rotation: '90deg' },
+        initial: null
+    })
 
     return (
         <div className="flex flex-row bg-slate-900 grow justify-center align-middle items-center text-white gap-36">
@@ -22,16 +31,32 @@ function Counter({ count, setCount }) {
                 <MinusIcon className="h-16 w-16" />
             </Button>
 
-            <div
-                className={"text-9xl " + (isError ? "text-red-500" : "text-white")}>
-                {count}
+            <div className="tabular-nums" style={{
+                width: String(count).length + "rem"
+            }}>
+                {
+                    transitions((style, item) => (
+                        <animated.div
+                            className={"text-9xl tabular-nums text-center " + (isError ? "text-red-500" : "text-white")}
+                            style={{
+                                height: "2rem",
+                                overflow: "visible",
+                                //width: (item / 10) + "rem",
+                                transform: to([style.position, style.rotation], (p, s) => `translateY(${p}) rotateX(${s})`),
+                                opacity: style.opacity
+                            }}>
+                            {item}
+                        </animated.div>
+                    ))
+                }
             </div>
 
             <Button className="h-24 w-24" variant="ghost" size="icon" onClick={function () { changeCount(1) }}>
                 <PlusIcon className="h-16 w-16" />
             </Button>
-        </div>
+        </div >
     );
 }
 
+// <animated.div className={"text-9xl tabular-nums " + (isError ? "text-red-500" : "text-white")} style={props}>{count}</animated.div>
 export default Counter;
