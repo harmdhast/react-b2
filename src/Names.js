aa8eda8a251659d901e8e25f2bd29ab3dca19adc
harmdhast <pandemonium.harmdhast@gmail.com>
2024-01-31 10:04:21 +0100

    refactor[Names]: Gestion des erreurs, séparation des fonctions

--- src/Names.js
+++ src/Names.js
@@ -1,31 +1,50 @@
-import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { animated, useSpringValue } from '@react-spring/web';
+import { useToast } from "./components/ui/use-toast";
 
-export function ButtonOutline() {
-    return;
-}
 function Names({ name, setName }) {
-    async function fetchName() {
+    const opacity = useSpringValue(1);
+    const { toast } = useToast();
+
+    async function getRandomName() {
         try {
             const response = await fetch("https://randomuser.me/api/");
             const data = await response.json();
 
-            opacity.set(0);
-            opacity.start(1);
-            const name = data.results[0].name;
-            setName(name.first + " " + name.last);
-        } catch (e) {
-            console.log(`Couldn't fetch name: ${e}`);
+            if (Object.hasOwn(data, "results") && data.results.length > 0) {
+                return data;
+            }
+            return null;
+        } catch (error) {
+            console.error(error);
+            return null;
         }
     }
 
-    const opacity = useSpringValue(1);
+    function playAnimation() {
+        opacity.set(0);
+        opacity.start(1);
+    }
+
+    async function setNewName() {
+        const data = await getRandomName();
+
+        if (!data) {
+            return toast({
+                title: "Erreur API: Aucune donnée",
+                variant: "destructive"
+            })
+        }
+
+        const name = data.results[0].name;
+        playAnimation();
+        setName(`${name.first} ${name.last}`);
+    }
 
     return (
         <div className="flex flex-col bg-slate-900 grow justify-center align-middle items-center text-white gap-1">
             <animated.div className="text-xl h-5 mb-2" style={{ opacity }}>{name}</animated.div>
-            <Button onClick={fetchName} variant="outline" className="text text-xl" size="lg">Changer nom</Button>
+            <Button onClick={setNewName} variant="outline" className="text text-xl" size="lg">Changer nom</Button>
         </div>
     );
 }
