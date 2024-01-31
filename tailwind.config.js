b3a4bcc19887fee3b72f03a37cdb1ca017073a98
harmdhast <pandemonium.harmdhast@gmail.com>
2024-01-31 11:48:01 +0100

    misc css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './src/**/*.{js,jsx}',
-  ],
-  prefix: "",
-  theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
-    fontFamily: {
-      sans: ['"Inter"', 'sans-serif']
-    },
-    extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+import colors, { indigo, yellow, gray } from 'tailwindcss/colors'
+export const darkMode = ["class"]
+export const content = [
+  './src/**/*.{js,jsx}',
+]
+export const prefix = ""
+export const theme = {
+  colors: {
+    ...colors,
+    primary: indigo,
+    secondary: yellow,
+    neutral: gray,
+  },
+  container: {
+    center: true,
+    padding: "2rem",
+    screens: {
+      "2xl": "1400px",
     },
   },
-  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
+  fontFamily: {
+    sans: ['"Inter"', 'sans-serif']
+  },
+  extend: {
+    keyframes: {
+      "accordion-down": {
+        from: { height: "0" },
+        to: { height: "var(--radix-accordion-content-height)" },
+      },
+      "accordion-up": {
+        from: { height: "var(--radix-accordion-content-height)" },
+        to: { height: "0" },
+      },
+    },
+    animation: {
+      "accordion-down": "accordion-down 0.2s ease-out",
+      "accordion-up": "accordion-up 0.2s ease-out",
+    },
+  },
 }
+export const plugins = [require("tailwindcss-animate"), require('@tailwindcss/typography')]
