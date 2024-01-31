daf5423a5cb45e8308634a1675ea51fb3ca13093
harmdhast <pandemonium.harmdhast@gmail.com>
2024-01-31 10:45:22 +0100

    fix: DarkMode localStorage

--- src/components/misc/darkMode.js
+++ src/components/misc/darkMode.js
@@ -6,20 +6,23 @@ function DarkModeSwitch() {
     const [isDarkMode, toggleDarkMode] = useState(null);
 
     useEffect(() => {
+        if (isDarkMode === null) {
+            return;
+        }
         document.body.classList.toggle('dark', isDarkMode);
         localStorage.setItem('darkmode', isDarkMode);
     }, [isDarkMode]);
 
     useEffect(() => {
+        // Récupération localStorage
+        const value = localStorage.getItem('darkmode');
+        if (value !== null) {
+            return toggleDarkMode(value === "true"); // localStorage = string
+        }
+
         // Utilisation du mode préféré si à la première connexion
         const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
         darkModePreference.addEventListener("change", e => toggleDarkMode(e.matches));
-
-        // Récupération localStorage
-        const darkMode = localStorage.getItem('darkmode');
-        if (darkMode) {
-            toggleDarkMode(darkMode);
-        }
     }, []);
 
     return (
